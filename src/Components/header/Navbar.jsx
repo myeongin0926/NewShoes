b3651808072b7e46ae60cc457404f115ec658ab3
myeongin <mungin0926@naver.com>
2023-07-22 00:19:54 +0900

    header&navbar build

---
+++ src/Components/header/Navbar.jsx
@@ -0,0 +1,23 @@
+import Button from "../Button";
+import { styled } from "styled-components";
+
+const StyleNavbar = styled.nav`
+  display: flex;
+  gap: 100px;
+`;
+
+export default function Navbar() {
+  const navButtonData = [
+    { text: "Shoes", id: 1, param: "shoes" },
+    { text: "Outer", id: 2, param: "outer" },
+    { text: "Bag", id: 3, param: "bag" },
+    { text: "Pants", id: 4, param: "pants" },
+  ];
+  return (
+    <StyleNavbar>
+      {navButtonData.map((item) => (
+        <Button font="20" key={item.id} value={item.text} param={item.param} />
+      ))}
+    </StyleNavbar>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Button from "../Components/Button";
+export default function Home() {
+  return <div></div>;
+}
